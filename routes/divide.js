3d533e022de959dc7df3d5f85314c1a1f6bbd66b
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-10-14 16:53:49 +0200

    adds jsend to endpoints

--- routes/divide.js
+++ routes/divide.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var jsend = require('jsend');
+router.use(jsend.middleware);
 
 router.get('/:number1/:number2', function(req, res, next) {
-  res.status(200).json(parseInt(req.params.number1) / parseInt(req.params.number2));
+  const result = parseInt(req.params.number1) / parseInt(req.params.number2);
+  res.jsend.success(result);
 });
 
 module.exports = router;

c3168445e4791523d971ace50f83a6854adf639f
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2024-10-14 17:07:45 +0200

    adds error handling to endpoints

--- routes/divide.js
+++ routes/divide.js
@@ -4,8 +4,24 @@ var jsend = require('jsend');
 router.use(jsend.middleware);
 
 router.get('/:number1/:number2', function(req, res, next) {
-  const result = parseInt(req.params.number1) / parseInt(req.params.number2);
-  res.jsend.success(result);
+    const number1 = parseInt(req.params.number1);
+    if(isNaN(number1)) {
+        return res.jsend.fail({"number1": "number1 is not in correct format"});
+    }
+    const number2 = parseInt(req.params.number2);
+    if(isNaN(number2)) {
+        return res.jsend.fail({"number2": "number2 is not in correct format"});
+    }
+    if(number2 == 0) {
+        return res.jsend.fail({"number2": "number2 cannot be 0"});
+    }
+    const result = number1 / number2;
+    if (Number.isInteger(result)) {
+        res.jsend.success({"result": result});
+    }
+    else {
+        res.jsend.success({"result": Math.round(result), "message": "Result has been rounded, as it was not an integer."});
+    }
 });
 
 module.exports = router;
--- routes/subtract.js
+++ routes/subtract.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var jsend = require('jsend');
-router.use(jsend.middleware);
 
+router.use(jsend.middleware);
 router.get('/:number1/:number2', function(req, res, next) {
-  const result = parseInt(req.params.number1) - parseInt(req.params.number2);
-  res.jsend.success(result);
+    const number1 = parseInt(req.params.number1);
+    if(isNaN(number1)) {
+        return res.jsend.fail({"number1": "number1 is not in correct format"});
+    }
+    const number2 = parseInt(req.params.number2);
+    if(isNaN(number2)) {
+        return res.jsend.fail({"number2": "number2 is not in correct format"});
+    }
+    const result = number1 - number2;
+    res.jsend.success({"result": result});
 });
 
 module.exports = router;
